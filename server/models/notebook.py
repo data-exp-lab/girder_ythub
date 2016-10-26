cd49fccad0cdb369e93a63d341d5c3b4c8968ac9
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2016-10-26 09:16:34 -0500

    Store the ip of the host running the container, pass it as header in DELETE for routing

--- server/models/notebook.py
+++ server/models/notebook.py
@@ -73,11 +73,14 @@ class Notebook(AccessControlledModel):
             'containerId': str(notebook['containerId']),
             'containerPath': str(notebook['containerPath']),
             'mountPoint': str(notebook['mountPoint']),
+            'host': str(notebook['host']),
             'folderId': str(notebook['folderId']),
             'girder_token': str(token['_id']),
         }
+        headers = {'docker-host': str(notebook['host']),
+                   'content-type': 'application/json'}
         requests.delete(self.model('setting').get(PluginSettings.TMPNB_URL),
-                        json=payload)
+                        json=payload, headers=headers)
         # TODO: handle error
         self.remove(notebook)
 
@@ -159,6 +162,7 @@ class Notebook(AccessControlledModel):
             'containerId': nb['containerId'],
             'containerPath': nb['containerPath'],
             'mountPoint': nb['mountPoint'],
+            'host': nb['host'],
             'lastActivity': now,
             'status': NotebookStatus.RUNNING,   # be optimistic for now
             'created': now,
