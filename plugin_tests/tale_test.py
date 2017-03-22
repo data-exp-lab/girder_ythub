63ab2750a2aa29b93a665fac3d1fa0325f4605b9
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2017-03-22 10:41:52 -0500

    Add rudimentary tests for Tales

---
+++ plugin_tests/tale_test.py
@@ -0,0 +1,186 @@
+
+import httmock
+import json
+from tests import base
+from .tests_helpers import \
+    GOOD_REPO, GOOD_COMMIT, \
+    mockOtherRequest, mockCommitRequest, mockReposRequest
+
+
+def setUpModule():
+    base.enabledPlugins.append('wholetale')
+    base.startServer()
+
+
+def tearDownModule():
+    base.stopServer()
+
+
+class TaleTestCase(base.TestCase):
+
+    def setUp(self):
+        super(TaleTestCase, self).setUp()
+        users = ({
+            'email': 'root@dev.null',
+            'login': 'admin',
+            'firstName': 'Root',
+            'lastName': 'van Klompf',
+            'password': 'secret'
+        }, {
+            'email': 'joe@dev.null',
+            'login': 'joeregular',
+            'firstName': 'Joe',
+            'lastName': 'Regular',
+            'password': 'secret'
+        })
+        self.admin, self.user = [self.model('user').createUser(**user)
+                                 for user in users]
+        with httmock.HTTMock(mockReposRequest, mockCommitRequest,
+                             mockOtherRequest):
+            self.recipe = self.model('recipe', 'wholetale').createRecipe(
+                GOOD_COMMIT, 'https://github.com/' + GOOD_REPO,
+                creator=self.user, public=True)
+        self.image = self.model('image', 'wholetale').createImage(
+            self.recipe, GOOD_REPO, name="my name", creator=self.user,
+            public=True)
+
+    def testTaleFlow(self):
+        resp = self.request(
+            path='/tale', method='POST', user=self.user,
+            params={'imageId': str(self.image['_id'])}
+        )
+        self.assertStatus(resp, 400)
+        self.assertEqual(resp.json, {
+            'message': ('You need to specify either an "instanceId"'
+                        ' or an "imageId" and a "folderId".'),
+            'type': 'rest'
+        })
+
+        # Grab the default user folders
+        resp = self.request(
+            path='/folder', method='GET', user=self.user, params={
+                'parentType': 'user',
+                'parentId': self.user['_id'],
+                'sort': 'name',
+                'sortdir': 1
+            })
+        privateFolder = resp.json[0]
+        publicFolder = resp.json[1]
+
+        resp = self.request(
+            path='/folder', method='GET', user=self.admin, params={
+                'parentType': 'user',
+                'parentId': self.admin['_id'],
+                'sort': 'name',
+                'sortdir': 1
+            })
+        # adminPrivateFolder = resp.json[0]
+        adminPublicFolder = resp.json[1]
+
+        resp = self.request(
+            path='/tale', method='POST', user=self.user,
+            params={'imageId': str(self.image['_id']),
+                    'folderId': publicFolder['_id']}
+        )
+        self.assertStatusOk(resp)
+        tale = resp.json
+
+        resp = self.request(
+            path='/tale/{_id}'.format(**tale), method='PUT',
+            user=self.user, params={
+                'name': 'new name',
+                'description': 'new description',
+                'config': json.dumps({'memLimit': '2g'}),
+                'public': True,
+                'published': False
+            }
+        )
+        self.assertStatusOk(resp)
+        self.assertEqual(resp.json['name'], 'new name')
+        tale = resp.json
+
+        resp = self.request(
+            path='/tale', method='POST', user=self.user,
+            params={'imageId': str(self.image['_id']),
+                    'folderId': privateFolder['_id']}
+        )
+        self.assertStatusOk(resp)
+        new_tale = resp.json
+
+        resp = self.request(
+            path='/tale', method='POST', user=self.admin,
+            params={'imageId': str(self.image['_id']),
+                    'folderId': adminPublicFolder['_id'],
+                    'public': False}
+        )
+        self.assertStatusOk(resp)
+        # admin_tale = resp.json
+
+        resp = self.request(
+            path='/tale', method='GET', user=self.admin,
+            params={}
+        )
+        self.assertStatusOk(resp)
+        self.assertEqual(len(resp.json), 3)
+
+        resp = self.request(
+            path='/tale', method='GET', user=self.user,
+            params={'imageId': str(self.image['_id'])}
+        )
+        self.assertStatusOk(resp)
+        self.assertEqual(len(resp.json), 2)
+        self.assertEqual(set([_['_id'] for _ in resp.json]),
+                         {tale['_id'], new_tale['_id']})
+
+        resp = self.request(
+            path='/tale', method='GET', user=self.user,
+            params={'folderId': publicFolder['_id']}
+        )
+        self.assertStatusOk(resp)
+        self.assertEqual(len(resp.json), 1)
+        self.assertEqual(set([_['_id'] for _ in resp.json]),
+                         {tale['_id']})
+
+        resp = self.request(
+            path='/tale', method='GET', user=self.user,
+            params={'userId': str(self.user['_id'])}
+        )
+        self.assertStatusOk(resp)
+        self.assertEqual(len(resp.json), 2)
+        self.assertEqual(set([_['_id'] for _ in resp.json]),
+                         {tale['_id'], new_tale['_id']})
+
+        resp = self.request(
+            path='/tale', method='GET', user=self.user,
+            params={'text': 'new'}
+        )
+        self.assertStatusOk(resp)
+        self.assertEqual(len(resp.json), 1)
+        self.assertEqual(set([_['_id'] for _ in resp.json]),
+                         {tale['_id']})
+
+        resp = self.request(
+            path='/tale/{_id}'.format(**new_tale), method='DELETE',
+            user=self.admin)
+        self.assertStatusOk(resp)
+
+        resp = self.request(
+            path='/tale/{_id}'.format(**new_tale), method='GET',
+            user=self.user)
+        self.assertStatus(resp, 400)
+
+        resp = self.request(
+            path='/tale/{_id}'.format(**tale), method='GET',
+            user=self.user)
+        self.assertStatusOk(resp)
+        for key in tale.keys():
+            if key in ('access', 'updated', 'created'):
+                continue
+            self.assertEqual(resp.json[key], tale[key])
+
+    def tearDown(self):
+        self.model('user').remove(self.user)
+        self.model('user').remove(self.admin)
+        self.model('recipe', 'wholetale').remove(self.recipe)
+        self.model('image', 'wholetale').remove(self.image)
+        super(TaleTestCase, self).tearDown()
